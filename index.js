3c0f350ad8777126b780563308aa2d1f8f54dc37
Thiago de Melo Torres Lopes <thiagodemelo2001@hotmail.com>
2025-10-20 20:33:19 -0300

    Aula 20/10

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const cors = require('cors');
 
 const app = express(); 
 app.use(cors()); 
-// app.use(express.json()); 
+app.use(express.json()); 
 
-const porta = process.env.PORT || 3334;
+const porta = process.env.PORT || 3333;
 
 app.listen(porta, () => {
-    console.log(`Servidor iniciado em http://localhost:${porta}`);
+    console.log(`Servidor iniciado em http://${process.env.SERVER}:${porta}`);
 });
 
 app.get('/', (request, response) => {
