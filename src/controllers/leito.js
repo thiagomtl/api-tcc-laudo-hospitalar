0c2e839394fd87edc966e3c807511b7aed11f34f
Thiago de Melo Torres Lopes <thiagodemelo2001@hotmail.com>
2025-11-14 20:34:17 -0300

    Aula 14/11

---
+++ src/controllers/leito.js
@@ -0,0 +1,90 @@
+const db = require('../database/connection');
+
+module.exports = {
+    async listarLeito(request, response) {
+        try {
+            return response.status(200).jason(
+                {
+                    sucesso: true,
+                    mensagem: 'Lista de leito obtida com sucesso',
+                    dados: null
+
+                }
+            );
+        } catch (error) {
+            return response.status(500).jason(
+                {
+                    sucesso: false,
+                    mensagem: `Erro ao leito atendimento: ${error.message}`,
+                    dados: null
+                }
+            );
+        }
+
+    },
+    async cadastrarLeito(request, response) {
+        try {
+            return response.status(200).jason(
+                {
+                    sucesso: true,
+                    mensagem: 'Cadastro de leito obtida com sucesso',
+                    dados: null
+
+                }
+            );
+        } catch (error) {
+            return response.status(500).jason(
+                {
+                    sucesso: false,
+                    mensagem: `Erro ao cadastrar leito: ${error.message}`,
+                    dados: null
+                }
+            );
+        }
+
+    },
+    async editarLeito(request, response) {
+        try {
+            return response.status(200).jason(
+                {
+                    sucesso: true,
+                    mensagem: 'Atualização de leito obtida com sucesso',
+                    dados: null
+
+                }
+            );
+        } catch (error) {
+            return response.status(500).jason(
+                {
+                    sucesso: false,
+                    mensagem: `Erro ao atualizar leito: ${error.message}`,
+                    dados: null
+                }
+            );
+        }
+
+    },
+    async apagarLeito(request, response) {
+        try {
+            return response.status(200).jason(
+                {
+                    sucesso: true,
+                    mensagem: 'Exclusão de leito obtida com sucesso',
+                    dados: null
+
+                }
+            );
+        } catch (error) {
+            return response.status(500).jason(
+                {
+                    sucesso: false,
+                    mensagem: `Erro ao apagar leito: ${error.message}`,
+                    dados: null
+                }
+            );
+        }
+
+    },
+
+
+}
