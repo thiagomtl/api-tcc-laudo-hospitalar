942417a4114424e6335776bde83403fb8f7c9433
João Marcos França <joao.santos2356@etec.sp.gov.br>
2026-05-19 20:00:46 -0300

    19/05/2026

--- src/routes/thiago.js
+++ src/routes/thiago.js
@@ -7,6 +7,7 @@ const AtendimentoController = require('../controllers/atendimento');
 const ConvenioController = require('../controllers/convenio');
 const LeitoController = require('../controllers/leito');
 const LogsAcaoController = require("../controllers/logsAcao");
+const IntegracaoController = require('../controllers/integracao');
 
 router.get('/atendimento', AtendimentoController.listarAtendimento);
 router.get('/atendimento/listar-atend', AtendimentoController.listarAtend); // rota extra para obter lista ascendente de IDs
@@ -14,6 +15,8 @@ router.get('/atendimento/pendentes', AtendimentoController.listarAtendimentosPen
 router.post('/atendimento', AtendimentoController.cadastrarAtendimento);
 router.patch('/atendimento/:id', AtendimentoController.editarAtendimento);
 router.delete('/atendimento/:id', AtendimentoController.apagarAtendimento);
+
+router.post('/integracao/atendimento-pendente', IntegracaoController.criarAtendimentoPendente);
 
 router.get('/convenio', ConvenioController.listarConvenio);
 router.post('/convenio', ConvenioController.cadastrarConvenio);
